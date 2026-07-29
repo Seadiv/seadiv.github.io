23d84ce17206a6ad1082e0a2e3d6c904e3e0257d
Seadiv <snowrealms@gmail.com>
2026-07-30 00:21:08 +0200

    changes that may or may not break some things

--- media/js/hamburgerjs.js
+++ media/js/hamburgerjs.js
@@ -1,9 +1,9 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function hamburgerjs() {
   var x = document.getElementById("menu-page");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 } 
